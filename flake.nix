355d9b5d6f940534a73bdc240364892f11e43c60
pxzc1 <tonaok2555@gmail.com>
2026-02-28 12:09:07 +0700

    update flake.nix: add zen-browser.url

--- flake.nix
+++ flake.nix
@@ -4,11 +4,16 @@
   inputs = {
     # Using the same version as your stateVersion
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    #zen browser
+    zen-browser.url = "github:youwen5/zen-browser-flake";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+
+      specialArgs = { inherit inputs };
       modules = [
         # imports hardware detection
         ./hardware-configuration.nix
